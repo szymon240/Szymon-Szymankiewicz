39aac1be18e1c5685b3336eefc898e985326453d
szymon240 <115163248+szymon240@users.noreply.github.com>
2022-12-01 16:58:31 +0100

    Create ex-1.cpp

---
+++ lesson-6/ex-1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+class Number {
+private:
+    int a;
+    int b;
+public:
+    Number(int x, int y);
+
+    int max() {
+        return a > b ? a : b;
+    }
+
+    int min() {
+        return a < b ? a : b;
+    }
+
+    float avg() {
+        return (a+b)/2;
+    }
+};
+
+Number::Number(int x, int y) {
+    a = x;
+    b = y;
+};
+
+int main() {
+    Number number(1, 3);
+    int a = number.max();
+    int b = number.min();
+    float c = number.avg();
+    std::cout<<"Max: "<<a<<" Min: "<<b<<" Avg "<<c<<std::endl;
+    return 0;
+}

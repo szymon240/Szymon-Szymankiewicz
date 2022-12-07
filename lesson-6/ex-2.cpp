c453e4ce3dedbf9c7152c4a064505971e590da26
szymon240 <115163248+szymon240@users.noreply.github.com>
2022-12-07 12:10:10 +0100

    Update ex-2.cpp

--- lesson-6/ex-2.cpp
+++ lesson-6/ex-2.cpp
@@ -9,30 +9,25 @@ private:
 public:
     double doubleValue() { return log(this->argument) / log(this->base);}
     Logarithm(double inputBase, double inputArgument) {
-        try {
-            if (inputBase < 0) throw 1;
-            if (inputBase == 1) throw 2;
-            if (inputArgument < 0) throw 3;
+            if (inputBase < 0) throw "Wrong base! Base must be higher than 0.";
+            if (inputBase == 1) throw "Wrong base! Base can't be equal to 1.";
+            if (inputArgument < 0) throw "Wrong argument! Argument must be higher than 0.";
             this->base = inputBase;
             this->argument = inputArgument;
             std::cout<<inputBase<<" "<<inputArgument<<"\n";
-
-        }
-        catch (int a) {
-            if (a == 1) {std::cout<<"Wrong base! Base must be higher than 0."<<std::endl;}
-            if (a == 2) {std::cout<<"Wrong base! Base can't be equal to 1."<<std::endl;}
-            if (a == 3) {std::cout<<"Wrong argument! Argument must be higher than 0."<<std::endl;}
-
-        }
     }
 };
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    Logarithm a(2,3);
-    Logarithm b(-3,4);
-    Logarithm c(1,5);
-    Logarithm d(3,-5);
+    try {
+        Logarithm a(2,3);
+        Logarithm b(-3,4);
+        Logarithm c(1,5);
+        Logarithm d(3,-5);
+    }
+    catch(char const* e){
+        std::cout<<e<<std::endl;
+    }
     return 0;
 }
